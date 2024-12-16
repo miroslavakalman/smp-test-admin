6a84967235c78735002f3de9d4e36340436360e6
Miroslava <miladygirllife@gmail.com>
2024-12-16 15:15:43 +0300

    styles&assets

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Админ-панель</title>
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+</head>
+<body>
+    <nav>
+        <img src="img/smp-logo.png" alt="Логотип SMP Esports">
+        <button class="object-choice">Дубровка <img src="img/arrow.svg" alt=""></button>
+        <button class="end-session">Выйти</button>
+    </nav>
+    <h1>Управление бронированиями. <br><span class="object-name">Дубровка</span></h1>  <!-- задел под разные объекты -->
+    <div class="filter-by-date">
+            
+    </div>
+</body>
+</html>
