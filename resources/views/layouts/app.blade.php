5c2be83f5a3242c049c181facfee596510196964
Miroslava <miladygirllife@gmail.com>
2024-12-16 16:26:55 +0300

    nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,8 @@
     <nav>
     <img src="{{ asset('img/smp-logo.png') }}" alt="Логотип SMP Esports">
     <button class="object-choice">Дубровка <img src="img/arrow.svg" alt=""></button>
-        <button class="end-session">Выйти</button>
+    <p><a href="https://google.com">Смены</a></p>
+    <button class="end-session">Выйти</button>
     </nav>
     <main>
     @yield('content')
